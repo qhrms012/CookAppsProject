da4f1bdee65974ddfc72b163de056c7f3a80b103
boguen <qhrms012@naver.com>
2025-09-30 17:31:00 +0900

    [add] jackbox 프리팹 구현

--- Assets/01.Scripts/Tile/BoardSpawner.cs
+++ Assets/01.Scripts/Tile/BoardSpawner.cs
@@ -8,13 +8,18 @@ public class HexBoardSpawner : MonoBehaviour
 {
     [Header("Tilemap")]
     public Tilemap bgTilemap;           // 하트 모양으로 칠해둔 배경 타일맵
+    public Tilemap jackBoxTilemap;
 
     [Header("Blocks")]
     public Block[] blockPrefabs;        // 블럭 프리팹(색상별)
 
+    [Header("JackBox")]
+    public GameObject jackBoxPrefab;
+
     void Start()
     {
         SpawnBlocks();
+        SpawnJackBoxesFromTile();
     }
 
     void SpawnBlocks()
@@ -40,6 +45,29 @@ public class HexBoardSpawner : MonoBehaviour
 
                     // Block 스크립트에 GridPos 저장해두면 추후 매칭/드랍에 유용
                     b.Init(b.color, new Vector2Int(x, y));
+                }
+            }
+        }
+    }
+
+    void  SpawnJackBoxesFromTile()
+    {
+        BoundsInt bounds = jackBoxTilemap.cellBounds;
+
+        for(int x = bounds.xMin;x < bounds.xMax; x++)
+        {
+            for(int y = bounds.yMin;y < bounds.yMax; y++)
+            {
+                Vector3Int cellPos = new Vector3Int(x, y, 0);
+
+                if (jackBoxTilemap.HasTile(cellPos))
+                {
+
+                    Vector3 worldPos = jackBoxTilemap.GetCellCenterWorld(cellPos);
+
+                    Instantiate(jackBoxPrefab, worldPos, Quaternion.identity);
+                    jackBoxTilemap.SetTile(cellPos, null);
+
                 }
             }
         }
